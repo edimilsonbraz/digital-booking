2d8e031503ef1f6aaa96e800d601a571425293ed
Renan Miranda <renanmirandae@gmail.com>
2023-03-06 10:47:29 -0300

    Atualização da pagina de produto

--- front-end/digital-booking/src/pages/Product/index.jsx
+++ front-end/digital-booking/src/pages/Product/index.jsx
@@ -1,10 +1,39 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import style from './style.module.css';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
 
 export function Product() {
-  const { id } = useParams()
-  return (
-    <section className="ContainerProduct">
-      <h1>Page product/${id}</h1>
-    </section>
-  )
+    const { id } = useParams();
+
+    const [categoria, setCategoria] = useState('');
+    const [titulo, setTitulo] = useState('');
+    const [localizacao, setLocalizacao] = useState('');
+
+    useEffect(() => {
+    }, [id])
+
+    return (
+        <section className="ContainerProduct">
+            <div className={style.headerdetails}>
+                <div className={style.title}>
+                    <span>{categoria}</span>
+                    <h1>{titulo}</h1>
+                </div>
+                <div className={style.backpage}>
+                    <Link to='/'>
+                        <section>
+                            <FontAwesomeIcon icon={faChevronLeft} size='3x' />
+                        </section>
+                    </Link>
+                </div>
+            </div>
+
+            <div className={style.locationdetails}>
+                <p><FontAwesomeIcon icon={faLocationDot} /> {localizacao}</p>
+            </div>
+        </section>
+    )
 }
