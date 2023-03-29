feccde643ca2c132cd37304072416555d363e761
Taynan Vitor Martins Souza <taynanvitorms@hotmail.com>
2023-03-29 23:59:24 +0000

    Reinderizando Header com botão de logout

--- front-end/digital-booking/src/components/Header/index.jsx
+++ front-end/digital-booking/src/components/Header/index.jsx
@@ -1,18 +1,41 @@
-import { NavLink } from 'react-router-dom'
+import { Navigate, NavLink } from 'react-router-dom'
 import { ToggleMenu } from '../ToggleMenu';
 import { useState } from 'react';
 import styles from './styles.module.css'
 import logo1 from '../../assets/logo1.svg'
+import { useEffect } from 'react';
+
 
 export function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [nome, setNome] = useState('');
 
   // Função para lidar com o logout do usuário
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // o token existe, o usuário está autenticado
+      setIsLoggedIn(true);
+      const nomeUsuario = obterNomeUsuario(token);
+      setNome(nomeUsuario);
+    }
+  }, []);
+
+  function obterNomeUsuario(token) {
+    // faça a lógica para obter o nome do usuário a partir do token
+    return 'João da Silva'; // apenas um exemplo
+  };
+
+  
+
+  // Função para lidar com o logout do usuário
   function handleLogout() {
+    localStorage.removeItem('token');
     setIsLoggedIn(false);
     setNome('');
+    window.location.reload();
   }
+
 
   return (
     <>
