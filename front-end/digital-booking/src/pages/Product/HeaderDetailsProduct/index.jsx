3a8a54a8a3f21fefc1d42e12d80ca30d61330554
edimilson braz <edimilson.gt8@gmail.com>
2023-04-12 13:00:26 -0300

    :sparkles: Refatorando HeaderDetailsProduct

---
+++ front-end/digital-booking/src/pages/Product/HeaderDetailsProduct/index.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom'
+import style from './style.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
+export function HeaderDetailsProduct({ newProduct }) {
+  console.log(newProduct)
+  return (
+    <>
+      <div className={style.headerdetails}>
+        <div className={style.title}>
+          <div>
+            {/* <span>{newProduct.categoria.descricaoCategoria}</span> */}
+          </div>
+          <h1>{newProduct.nomeProduto}</h1>
+        </div>
+        <div className={style.backpage}>
+          <Link to="/">
+            <section>
+              <FontAwesomeIcon icon={faChevronLeft} size="3x" />
+            </section>
+          </Link>
+        </div>
+      </div>
+
+      <div className={style.locationdetails}>
+        <FontAwesomeIcon icon={faLocationDot} />
+        {/* <p>{newProduct.cidades.nomeCidade}</p> */}
+        {/* <span>{newProduct.cidades.pais}</span>  */}
+      </div>
+    </>
+  )
+}
