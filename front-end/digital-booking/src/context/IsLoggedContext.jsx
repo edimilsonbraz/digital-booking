d4a361a35738e5f0aa7ecd5ca92aeb5609bf1685
Taynan Vitor Martins Souza <taynanvitorms@hotmail.com>
2023-04-12 01:46:53 +0000

    Usuario Logado

--- front-end/digital-booking/src/context/IsLoggedContext.jsx
+++ front-end/digital-booking/src/context/IsLoggedContext.jsx
@@ -1,10 +1,20 @@
 import { createContext, useState } from "react";
+import { useEffect } from 'react';
 
 export const IsLoggedContext = createContext();
 
 export const IsLoggedProvider = ({children}) =>
 {
     const [isLogged, setIsLogged] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+          // o token existe, o usuário está autenticado
+          setIsLogged(true);
+        }
+    }, []);
+
 
     const toggleIsLogged = () =>
     {
